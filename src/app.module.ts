053905faa9ab3db46d8d8c43e4264d0ac15a36b4
EliRodrEs <elisabetre.main@gmail.com>
2024-09-22 21:09:54 +0200

    fix: remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LibraryModule } from './library/library.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [LibraryModule, BooksModule],
+  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
