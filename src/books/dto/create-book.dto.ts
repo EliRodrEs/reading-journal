b16899532ba36a69e380b2ebe90412e2230eed2f
EliRodrEs <elisabetre.main@gmail.com>
2024-09-29 23:13:27 +0200

    feat: Add new Dto for books creation

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,4 +1,33 @@
 export class CreateBookDto {
-    title: string
-    author: string
+    title: string;
+    author: string;
+    genres: Genres[];
+    hasRepresentation: boolean;
+    tier: Tiers
+}
+
+export enum Genres {
+    FICTION = 'Fiction',
+    NON_FICTION = 'Non-fiction',
+    MYSTERY = 'Mystery',
+    FANTASY = 'Fantasy',
+    SCIENCE_FICTION = 'Science Fiction',
+    ROMANCE = 'Romance',
+    HORROR = 'Horror',
+    BIOGRAPHY = 'Biography',
+    HISTORY = 'History',
+    POETRY = 'Poetry',
+    THRILLER = 'Thriller',
+    SELF_HELP = 'Self Help',
+    ADVENTURE = 'Adventure'
+}
+
+export enum Tiers {
+    GOD_TIER = 'God tier',
+    GREAT = 'Great',
+    COOL = 'Cool',
+    OKAYISH = 'Okayish',
+    MEEEH = 'Meeeeh',
+    BAD = 'Bad',
+    DO_NOT_OPEN = 'Recycle bin material'
 }
